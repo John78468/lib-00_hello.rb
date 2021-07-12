c74096edf75e467437083372d0be4ff5db6ed1c1
John78468 <73955607+John78468@users.noreply.github.com>
2021-07-13 00:27:42 +0200

    Add files via upload

---
+++ 01_temperature.rb
@@ -0,0 +1,13 @@
+def ftoc(fahrenheit_degrees)
+    celsius = (fahrenheit_degrees.to_i - 32) * 5.0 / 9.0
+    celsius.round
+  end
+  
+  def ctof(celsius_degrees)
+    fahrenheit = (celsius_degrees.to_i * 9 / 5) + 32
+    fahrenheit.round
+  end
+
+  def ftoc(f)
+    return ((f - 32) / 1.8).to_i
+   end
